63944b2eeedc4987ac5f64fa26099085dde91ab0
Ashley Lowry <alowry@redventures.com>
2020-01-24 16:43:55 -0600

    test an alert

--- e2e/cypress/integration/first.spec.ts
+++ e2e/cypress/integration/first.spec.ts
@@ -1,8 +1,17 @@
 describe("<App/>", () => {
     before(() => {
-        cy.visit("http://localhost:8080");
+        cy.visit('/');
     });
     it("Has a Header", () => {
         cy.get("header").should("have.length", 1);
+    });
+    it("has a button that is clicky clicky clickable", () => {
+        const stub = cy.stub();
+        cy.on ('window:alert', stub);
+        cy.get('button').contains('I am a button').click()
+            .then(() => {
+                expect(stub.getCall(0)).to.be.calledWith('hi');
+            })
+
     });
 });
